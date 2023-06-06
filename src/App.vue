bebf0f4120ff3398c8060a8ea74348babd8b2d90
aosocram <marcosoave15@gmail.com>
2023-06-06 14:12:35 +0200

    Funzionamento di tutti gli "Sposta in"

--- src/App.vue
+++ src/App.vue
@@ -13,28 +13,33 @@
             <button @click="aggiungiTask" class="aggiungiBtn">Aggiungi</button>
           </a>
           <a @click="aggiungiPuls()" class="bordoIntero"
-            :class="{ riduci: !aggiungiBool }, { riduciAggiungi: aggiungiBool }" href="#">Aggiungi</a>
+            :class="{ riduci: !aggiungiBool }, { riduciAggiungi: aggiungiBool }, { sfondochiaro: !aggiungiBool }, { sfondoscuro: aggiungiBool }"
+            href="#">Aggiungi</a>
 
         </div>
 
-        <a class="ml-auto bordoIntero" :class="{ riduciRimuovi: aggiungiBool }" @click="rimuoviPuls()"
-          href="#">Rimuovi</a>
+        <a class="ml-auto bordoIntero"
+          :class="{ riduciRimuovi: aggiungiBool }, { sfondochiaro: !rimuoviBool }, { sfondoscuro: rimuoviBool }"
+          @click="rimuoviPuls()" href="#">Rimuovi</a>
 
         <div class="group">
-          <a v-if="fattoIncorso" class="fattoBtn fattoBtnPrim riduciBottone" href="#">Fatto</a>
-          <a @click="incorsoPuls" class="bordoIntero" :class="{ riduci: !fattoIncorso }" href="#">Sposta in
-            "In
-            corso"</a>
+          <a v-if="fattoIncorso" class="fattoBtn fattoBtnPrim riduciBottone" @click="spostaincorsoFatto()">Fatto</a>
+          <a @click="incorsoPuls" class="bordoIntero"
+            :class="{ riduci: !fattoIncorso }, { sfondochiaro: !fattoIncorso }, { sfondoscuro: fattoIncorso }"
+            href="#">Sposta in
+            "In corso"</a>
         </div>
         <div class="group">
-          <a v-if="fattoCompletati" class="fattoBtn riduciBottone" href="#">Fatto</a>
-          <a @click="completatiPuls" class="bordoIntero" :class="{ riduci: !fattoCompletati }" href="#">Sposta
-            in "Completati"</a>
+          <a v-if="fattoCompletati" class="fattoBtn riduciBottone" @click="spostacompletatiFatto()" href="#">Fatto</a>
+          <a @click="completatiPuls" class="bordoIntero"
+            :class="{ riduci: !fattoCompletati }, { sfondochiaro: !fattoCompletati }, { sfondoscuro: fattoCompletati }"
+            href="#">Sposta in "Completati"</a>
         </div>
         <div class="group">
-          <a v-if="fattoDafare" class="fattoBtn riduciBottone" href="#">Fatto</a>
-          <a @click="dafarePuls" class="bordoIntero" :class="{ riduci: !fattoDafare }" href="#">Sposta in
-            "Da
+          <a v-if="fattoDafare" class="fattoBtn riduciBottone" @click="spostadafareFatto()" href="#">Fatto</a>
+          <a @click="dafarePuls" class="bordoIntero"
+            :class="{ riduci: !fattoDafare }, { sfondochiaro: !fattoDafare }, { sfondoscuro: fattoDafare }"
+            href="#">Sposta in "Da
             fare"</a>
         </div>
 
@@ -43,38 +48,41 @@
   </nav>
 
   <div class="taskContainer">
-    <div style="width: 250px;margin-top: 9px; text-align: center; height:679px;color: white;">
+    <div style="width: 250px;margin-top: 9px; text-align: center; height:670px;color: white;">
       DA FARE
       <hr>
       <div class="containerTFS">
         <ul>
-          <li v-for="t in dafareTasks">
-            {{ t.task }} <button @click="rimuoviTask(t)" v-if="rimuoviBool" class="rimuoviBtn"></button>
+          <li v-for="t in dafareTasks" :class="{ rmStyle: incorsoBool }">
+            <input class="checkbox" v-if="incorsoBool" type="checkbox" v-model="t.spostaincorso"> {{ t.task }} <button
+              @click="rimuoviTask(t)" v-if="rimuoviBool" class="rimuoviBtn"></button>
           </li>
         </ul>
       </div>
     </div>
 
     <div
-      style="width: 250px;margin-top: 9px;border-left: 3px solid #424c57; border-right: 3px solid #424c57;text-align: center; height:679px;color: white;">
+      style="width: 250px;margin-top: 9px;border-left: 3px solid #424c57; border-right: 3px solid #424c57;text-align: center; height:670px;color: white;">
       IN CORSO
       <hr>
       <div class="containerTFS">
         <ul>
-          <li v-for="t in incorsoTasks">
-            {{ t.task }}{{ t.task }} <button @click="rimuoviTask(t)" v-if="rimuoviBool" class="rimuoviBtn"></button>
+          <li v-for="t in incorsoTasks" :class="{ rmStyle: completatiBool || dafareBool }">
+            <input class="checkbox" v-if="completatiBool" type="checkbox" v-model="t.spostacompletati"> <input
+              class="checkbox" v-if="dafareBool" type="checkbox" v-model="t.spostadafare"> {{ t.task }}
+            <button @click="rimuoviTask(t)" v-if="rimuoviBool" class="rimuoviBtnIncorso"></button>
           </li>
         </ul>
       </div>
     </div>
 
-    <div style="width: 250px;margin-top: 8.5px; text-align: center; height:679px;color: white;">
+    <div style="width: 250px;margin-top: 8.5px; text-align: center; height:670px;color: white;">
       COMPLETATI
       <hr>
       <div class="containerTFS">
         <ul>
           <li v-for="t in completatiTasks">
-            {{ t.task }}{{ t.task }} <button @click="rimuoviTask(t)" class="rimuoviBtn"></button>
+            {{ t.task }} <button @click="rimuoviTask(t)" class="rimuoviBtn"></button>
           </li>
         </ul>
       </div>
@@ -85,7 +93,7 @@
 export default {
   data() {
     return {
-      tasks: [{ task: "Cambiare i cuscinetti", dafare: true, incorso: false, completati: false }, { task: "Cambiare l'olio", dafare: true, incorso: false, completati: false }],
+      tasks: [{ task: "Cambiare i cuscinetti", dafare: true, incorso: false, completati: false, spostaincorso: false, spostacompletati: false, spostadafare: false }, { task: "Cambiare l'olio", dafare: true, incorso: false, completati: false, spostaincorso: false, spostacompletati: false, spostadafare: false }],
       tendinaShow: false,
       taskText: "",
       imageURL: "src/assets/pulsanteGIU.PNG",
@@ -93,7 +101,10 @@ export default {
       fattoCompletati: false,
       fattoDafare: false,
       aggiungiBool: false,
-      rimuoviBool: false
+      rimuoviBool: false,
+      incorsoBool: false,
+      dafareBool: false,
+      completatiBool: false,
     }
   },
   computed: {
@@ -129,6 +140,9 @@ export default {
       this.fattoDafare = false;
       this.aggiungiBool = false;
       this.rimuoviBool = false;
+      this.incorsoBool = false;
+      this.dafareBool = false;
+      this.completatiBool = false;
 
     },
     aggiungiPuls() {
@@ -137,6 +151,9 @@ export default {
       this.fattoCompletati = false;
       this.fattoDafare = false;
       this.rimuoviBool = false;
+      this.incorsoBool = false;
+      this.dafareBool = false;
+      this.completatiBool = false;
 
     },
     rimuoviPuls() {
@@ -145,36 +162,80 @@ export default {
       this.fattoCompletati = false;
       this.aggiungiBool = false;
       this.fattoDafare = false;
+      this.incorsoBool = false;
+      this.dafareBool = false;
+      this.completatiBool = false;
     },
 
     incorsoPuls() {
       this.fattoIncorso = !this.fattoIncorso;
+      this.incorsoBool = !this.incorsoBool;
       this.fattoCompletati = false;
       this.fattoDafare = false;
       this.aggiungiBool = false;
       this.rimuoviBool = false;
+      this.completatiBool = false;
+      this.dafareBool = false;
+
 
     },
     completatiPuls() {
       this.fattoCompletati = !this.fattoCompletati;
+      this.completatiBool = !this.completatiBool;
       this.fattoIncorso = false;
+      this.incorsoBool = false;
       this.fattoDafare = false;
       this.aggiungiBool = false;
       this.rimuoviBool = false;
+      this.dafareBool = false;
     },
     dafarePuls() {
       this.fattoDafare = !this.fattoDafare;
+      this.dafareBool = !this.dafareBool;
       this.fattoIncorso = false;
+      this.incorsoBool = false;
       this.fattoCompletati = false;
       this.aggiungiBool = false;
       this.rimuoviBool = false;
+      this.completatiBool = false;
+
     },
     aggiungiTask() {
-      if (this.taskText.length != 0) this.tasks.push({ task: this.taskText, dafare: true, incorso: false, completati: false })
+      if (this.taskText.length != 0) this.tasks.push({ task: this.taskText, dafare: true, incorso: false, completati: false, spostaincorso: false, spostacompletati: false, spostadafare: false })
       this.taskText = ''
     },
     rimuoviTask(e) {
       this.tasks = this.tasks.filter((t) => t !== e)
+    },
+    spostaincorsoFatto() {
+      for (const t in this.tasks) {
+        if (this.tasks.hasOwnProperty(t) && this.tasks[t].spostaincorso === true) {
+          this.tasks[t].spostaincorso = false;
+          this.tasks[t].dafare = false;
+          this.tasks[t].incorso = true;
+        }
+      }
+      this.tendina();
+    },
+    spostacompletatiFatto() {
+      for (const t in this.tasks) {
+        if (this.tasks.hasOwnProperty(t) && this.tasks[t].spostacompletati === true) {
+          this.tasks[t].spostacompletati = false;
+          this.tasks[t].incorso = false;
+          this.tasks[t].completati = true;
+        }
+      }
+      this.tendina();
+    },
+    spostadafareFatto() {
+      for (const t in this.tasks) {
+        if (this.tasks.hasOwnProperty(t) && this.tasks[t].spostadafare === true) {
+          this.tasks[t].incorso = false;
+          this.tasks[t].spostadafare = false;
+          this.tasks[t].dafare = true;
+        }
+      }
+      this.tendina();
     }
   }
 }
