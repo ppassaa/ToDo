0c6e3914c0e848d2fdbcb282469ab10eddd294c1
®Passa↓ <passariniluca05@gmail.com>
2023-06-15 09:25:02 +0200

    aggiunte tutte le immagini a vite.config
    ora il npm run build funziona

--- vite.config.js
+++ vite.config.js
@@ -13,8 +13,26 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ["src/assets/*"]
+      external: [
+      "src/assets/aggiungi.PNG",
+      "src/assets/bloccato.PNG",
+      "src/assets/conferma.PNG",
+      "src/assets/favicon.ico",
+      "src/assets/img1.PNG",
+      "src/assets/img2.PNG",
+      "src/assets/img3.PNG",
+      "src/assets/img4.PNG",
+      "src/assets/img5.PNG",
+      "src/assets/img6.PNG",
+      "src/assets/img7.PNG",
+      "src/assets/img8.PNG",
+      "src/assets/img9.PNG",
+      "src/assets/img10.PNG",
+      "src/assets/info.png",
+      "src/assets/rimuovi.PNG",
+      "src/assets/sbloccato.PNG",
+      "src/assets/trash.png",
+      ]
     }
   }
-  
 })
