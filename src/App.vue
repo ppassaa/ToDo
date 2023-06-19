37d70108425e22b793b72c9da656cb2ca394ff9f
Reyunityone <94675908+Reyunityone@users.noreply.github.com>
2023-06-19 09:25:22 +0200

    sistemate checkbox pt 2

--- src/App.vue
+++ src/App.vue
@@ -287,7 +287,7 @@
                 <p class="testoTask" >{{ t.task }}</p>
                 <div style="max-height:35px;display: flex; align-items: center;justify-content: space-between;">
                   <div style="flex-grow: 1;">
-                    <input type="checkbox" style="margin-left: 10px;width: 20px;height: 20px;" v-model="t.selezionatoDel" v-if="showCheckbox" @change="salvaselezione()">
+                    <input type="checkbox" style="margin-left: 10px;width: 20px;height: 20px;" v-model="t.selezionatoDel" v-if="showCheckbox" :class="{zindexBasso : showCheckbox, zindexAlto : !showCheckbox}" @change="salvaselezione()" @click.stop>
                   </div>
                   <div style="width: 100%; text-align: right;">
                     <p style="margin-right: 4%;font-size: small; margin-top: 15px;">Scadenza: {{ t.dataScadenza }} <p style="margin-right: 1%;">{{ `${t.nome} ${t.cognome}` }}</p></p>
@@ -318,7 +318,7 @@
                 <p class="testoTask">{{ t.task }}</p>
                 <div style="max-height:35px;display: flex; align-items: center;justify-content: space-between;">
                   <div style="flex-grow: 1;">
-                    <input type="checkbox" style="margin-left: 10px;width: 20px;height: 20px;" v-model="t.selezionatoDel" v-if="showCheckbox" @change="salvaselezione()">
+                    <input type="checkbox" style="margin-left: 10px;width: 20px;height: 20px;" v-model="t.selezionatoDel" v-if="showCheckbox" :class="{zindexBasso : showCheckbox, zindexAlto : !showCheckbox}" @change="salvaselezione()" @click.stop>
                   </div>
                   <div style="width: 70%;margin-right: 10px;text-align: right;">
                     <p style="margin-right: 4%;font-size: small; text-align: right; margin-top: 15px;">Scadenza: {{ t.dataScadenza }} <p style="margin-right: 1%;">{{ `${t.nome} ${t.cognome}` }}</p></p>
