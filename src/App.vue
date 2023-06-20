2cf8bf0d52da03d88941a6cacb2f6853d9d0480a
aosocram <marcosoave15@gmail.com>
2023-06-20 11:00:54 +0200

    sistemato caratteri grandi in gruppi

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <div class="" style="color: white;display: flex;width: 100%; height: 6.5%; min-height: 30px;padding: 10px">
     <div class="dropdown" :class="{dropdownhover: clickTendina}" @click="clickTendina=!clickTendina;cambiaFreccia()">
         <div style="display: flex; height: 100%; padding: 2px;">
-          <div>
+          <div style="height: 100%;overflow-y: auto;">
             {{ gruppi.find(g => g.id == currentGroup) == undefined ? "Caricando..." : gruppi.find(g => g.id == currentGroup).nome}}
           </div>
           <div id="freccia" style="color:white; margin-left: auto;">{{ freccia }}</div>
